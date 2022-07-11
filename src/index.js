453bd56ca301010976b750410b20fffce7260a3e
Gega <gega@Gegas-MacBook-Air.local>
2022-07-11 16:39:45 +0400

    adding forms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react"; 
 import  ReactDOM  from "react-dom/client";
 import { Provider } from "react-redux";
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware, compose } from '@reduxjs/toolkit';
 
 import App from "./components/App";
 import reducers from './reducers';
 
-const store = configureStore({ reducer: reducers })
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = configureStore({ reducer: reducers}, composeEnhancers(applyMiddleware()));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
